10d82126bcc38d1cf7373e9c1b8eb99a2d44b88d
Pontus Stjernström <pontus@stjernstrom.nu>
2017-09-28 22:59:38 +0200

    Removed dependency to react-game-kit and doing it natively in canvas element. It does not seem very hard at all.

--- electron/app/components/game/Game.tsx
+++ electron/app/components/game/Game.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
-import { Loop, Stage } from 'react-game-kit';
 
 // let styles = require('./Home.scss');
 
 export default class Game extends React.Component {
-  render() {
+
+componentDidMount() {
+    //var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
+    //const context = (HTMLCanvasElement)this.refs.canvas.getContext('2d')
+    //this.setState({ context: context });
+    //this.startGame();
+    requestAnimationFrame(() => {console.log('Frame')});
+}
+
+render() {
     return (
-        <Loop>
-            <Stage>
-                // Game specific components go here
-            </Stage>
-        </Loop>
+        <div>
+            <canvas ref="canvas"
+                width="500"
+                height="500"
+            />
+        </div>
     );
   }
 }
