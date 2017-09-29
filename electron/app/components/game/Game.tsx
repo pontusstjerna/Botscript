abe774e22ba7912ccb1a7d7a09142c26ca5c2520
Pontus Stjernström <pontus@stjernstrom.nu>
2017-09-29 07:24:50 +0200

    Finally made a successful canvas element and refering to it! Next up: some simple animation.

--- electron/app/components/game/Game.tsx
+++ electron/app/components/game/Game.tsx
@@ -1,25 +1,28 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
 // let styles = require('./Home.scss');
 
 export default class Game extends React.Component {
 
-componentDidMount() {
-    //var canvas = <HTMLCanvasElement>document.getElementById("mycanvas");
-    //const context = (HTMLCanvasElement)this.refs.canvas.getContext('2d')
-    //this.setState({ context: context });
-    //this.startGame();
-    requestAnimationFrame(() => {console.log('Frame')});
-}
+    componentDidMount() {
+        const canvas = (ReactDOM.findDOMNode(this.refs.canvas) as HTMLCanvasElement);
+        const context = canvas.getContext("2d");
+        this.setState({ context: context });
+        //this.startGame();
+        requestAnimationFrame(() => {console.log('Frame')});
+    }
 
-render() {
-    return (
-        <div>
-            <canvas ref="canvas"
-                width="500"
-                height="500"
-            />
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Botscript</h1>
+                <canvas ref="canvas"
+                    width="500"
+                    height="500"
+                    style={{background: '#303030'}}
+                />
+            </div>
+        );
+    }
 }
