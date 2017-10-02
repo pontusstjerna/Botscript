89f845869784fe1bb0a460c8674d0358e2159bbd
putzies <johanbrndtssn@gmail.com>
2017-10-03 00:53:41 +0200

    added first boilerplate, listen on port 3000

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+
+app.listen(PORT, () => {
+  console.log('Listening on port ' + PORT);
+})
